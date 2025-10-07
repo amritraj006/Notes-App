986fe9ef351a0f5bd96a9c401504b4e198ab128f
Amrit Raj <amritsingh5356@gmail.com>
2025-10-07 23:30:24 +0530

    latest commit

--- server/models/Comment.js
+++ server/models/Comment.js
@@ -1,12 +1,23 @@
 import mongoose from "mongoose";
 
 const commentSchema = new mongoose.Schema({
-  noteId: { type: mongoose.Schema.Types.ObjectId, ref: "Note", required: true },
-  userId: { type: String, required: true },
-  username: { type: String, required: true },
+  noteId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Note",
+    required: true,
+  },
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
   text: { type: String, required: true },
-  parentCommentId: { type: mongoose.Schema.Types.ObjectId, ref: "Comment", default: null }, // null if main comment
-  createdAt: { type: Date, default: Date.now },
-});
+  parentCommentId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Comment",
+    default: null,
+  },
+  replies: [{ type: mongoose.Schema.Types.ObjectId, ref: "Comment" }],
+}, { timestamps: true });
 
 export default mongoose.model("Comment", commentSchema);
--- server/models/Note.js
+++ server/models/Note.js
@@ -3,11 +3,13 @@ import mongoose from "mongoose";
 const noteSchema = new mongoose.Schema({
   title: { type: String, required: true },
   content: { type: String, required: true },
-  snippet: { type: String },
-  authorId: { type: String, required: true },
-  authorName: { type: String, required: true },
-  likes: { type: Number, default: 0 },
-  createdAt: { type: Date, default: Date.now },
-});
+  fileUrl: { type: String }, // optional for downloads
+  createdBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  likes: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+}, { timestamps: true });
 
 export default mongoose.model("Note", noteSchema);
